e792df02f9ea83d250ee8b3253c5b1f34d1321cc
Adria Tavares <adria.tavares28@gmail.com>
2022-04-03 11:10:53 -0300

    Exercício opcional - Day 2

--- Day2/challengeTwo.js
+++ Day2/challengeTwo.js
@@ -1,5 +1,17 @@
-const name = prompt("Qual é o seu nome?");
-const age = prompt("Quantos anos você tem?");
-const language = prompt("Qual linguagem de programação você está estudando?");
+const name = prompt("Qual é o seu nome?").trim();
+const age = prompt("Quantos anos você tem?").trim();
+const language = prompt("Qual linguagem de programação você está estudando?").trim();
 
 alert(`Olá ${name}, você tem ${age} anos e já está aprendendo ${language}!`);
+
+const like = Number(prompt(`Você gosta de estudar ${language}?\nResponda com o número 1 para SIM ou 2 para NÃO.`).trim())
+
+if (like === 1) {
+    alert("Muito bom! Continue estudando e você terá muito sucesso.");
+};
+if (like === 2) {
+    alert("Ahh que pena... Já tentou aprender outras linguagens?");
+};
+if (like !== 1 || like !== 2) {
+    alert(`Na próxima vez você me conta mesmo se gosta de ${language} ou não =)`)
+};
