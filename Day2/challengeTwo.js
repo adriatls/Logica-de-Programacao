71f071eb6067f2eeb1358b677044cf3614df3ec9
Adria Tavares <adria.tavares28@gmail.com>
2022-04-03 11:08:00 -0300

    Day 2

---
+++ Day2/challengeTwo.js
@@ -0,0 +1,5 @@
+const name = prompt("Qual é o seu nome?");
+const age = prompt("Quantos anos você tem?");
+const language = prompt("Qual linguagem de programação você está estudando?");
+
+alert(`Olá ${name}, você tem ${age} anos e já está aprendendo ${language}!`);
